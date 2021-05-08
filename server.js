d28f788371f2d18c4530a0dceec2908d6304841a
Mohian Dipta <mohianmostofa@gmail.com>
2021-05-09 00:33:25 +0600

    rest, login

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+const users = []
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        const user = {
+            name: req.body.name,
+            password: hashedPassword
+        }
+        users.push(user)
+        res.status(201).send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('success')
+        } else {
+            res.send('failed')
+        }
+    } catch (error) {
+        return res.status(500).send()
+    }
+})
+
+app.listen(3000)
